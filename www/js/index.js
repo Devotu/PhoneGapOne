599f02c5f1764a3c9e182fe372cfd913801f3fb0
Otuv <otuvesson@gmail.com>
2016-03-29 22:36:13 +0200

    Super basic JSON

--- www/js/index.js
+++ www/js/index.js
@@ -1,12 +1,22 @@
 function navigate(page) {
     if (page == 1){
+        var pageContentJson = getPageContent(page);
+        var pageContent = JSON.parse(pageContentJson);
+        document.getElementById("title").innerHTML = 
+            pageContent.title;
         document.getElementById("nav").innerHTML = 
-            "<p>This is the stuff of page1</p>";
+            pageContent.content;
     } else if (page == 2){
         document.getElementById("nav").innerHTML = 
             "<p>This is the page 2 stuff</p>";        
     } else {
         document.getElementById("nav").innerHTML = 
             "<p> Error in request</p>";
+    }
+}
+
+function getPageContent(page) {
+    if (page == 1){
+        return '{"title":"Page 1","content":"This is the page 1 content"}';
     }
 }
